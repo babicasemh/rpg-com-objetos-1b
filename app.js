596c70c99fdd7bd14fa6ddf2cb6c2147ee55061a
babica <babicasemh@gmail.com>
2026-08-27 07:44:11 -0300

    criamos os estados

---
+++ app.js
@@ -0,0 +1,38 @@
+class Personagem {
+    constructor(nome, vida, ataque){
+        this.nome = nome,
+        this.vida = vida,
+        this.ataque = ataque 
+    }
+
+    causaDano(personagem){
+        personagem.recebeDano(this.ataque)
+    }
+
+    recebeDano(quantidade){
+        this.vida = this.vida - quantidade
+
+        if(this.vida <= 0){
+            this.vida = "morreu"
+        }
+    }
+
+    estaVivo(){
+        return this.vida > 0
+    }
+
+    mostraStatus(){
+        console.log(this.nome + " | Vida: " + this.vida)
+    }
+}
+
+const druida = new Personagem("Obin", 150, 15)
+const guerreiro = new Personagem("Thorin", 60, 20)
+const mago = new Personagem("Gandalfe", 60, 35)
+const arqueiro = new Personagem("Legolas", 80, 25)
+
+mago.causaDano(arqueiro)
+mago.causaDano(arqueiro)
+druida.causaDano(arqueiro)
+arqueiro.mostraStatus()
+console.log(arqueiro.estaVivo())
